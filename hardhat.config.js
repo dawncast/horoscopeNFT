49e2e846b9060da60937421c7ae818ed3031a33f
dawncast <94773119+dawncast@users.noreply.github.com>
2023-06-16 14:31:53 -0700

    final deployed contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require('dotenv').config(); //all the key value pairs are being made available due to this lib
 require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+   
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.17",
+  defaultNetwork: 'goerli',
+  networks: {
+    hardhat: {},
+    goerli: {
+        url: process.env.API_URL_KEY,
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,53 +1,24 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-// const hre = require("hardhat");
-
-// async function main() {
-//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-//   const unlockTime = currentTimestampInSeconds + 60;
-
-//   const lockedAmount = hre.ethers.parseEther("0.001");
-
-//   const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-//     value: lockedAmount,
-//   });
-
-//   await lock.waitForDeployment();
-
-//   console.log(
-//     `Lock with ${ethers.formatEther(
-//       lockedAmount
-//     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-//   );
-// }
-
-// // We recommend this pattern to be able to use async/await everywhere
-// // and properly handle errors.
-// main().catch((error) => {
-//   console.error(error);
-//   process.exitCode = 1;
-// });
 const hre = require("hardhat"); 
-
+ 
 async function main() {  
-
-    // We get the contract to deploy  
+ 
     const horoscopeNFT = await hre.ethers.getContractFactory("horoscopeNFT");
     const hscp = await horoscopeNFT.deploy();   
     await hscp.deployed();
-    
-    console.log("horoscopeNFT deployed to:", hscp.address);   
- } 
  
-// We recommend this pattern to be able to use async/await
-// everywhere and properly handle errors.
+    //since we are testing, you should mention your own Eth wallet address
+    const myAddress= "0x4aFE042101541BDB9d9da802789d983C78564369";
+    console.log("horoscopeNFT deployed to:", hscp.address);   
+ 
+    let txn = await hscp.mintNFT(myAddress, 'virgo');
+    await txn.wait();
+ 
+}
+ 
 main()
     .then(() => process.exit(0))  
+
     .catch((error) => {    
     console.error(error);
-    process.exit(1);  
-});
+    process.exit(1); 
+ });
